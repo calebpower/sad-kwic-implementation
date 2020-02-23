9d953156be1ca24e6c5380a10d0911239732facb
Caleb L. Power <caleb.power@axonibyte.com>
2020-02-23 05:28:09 -0600

    added some additional tests snippets to the circular shift filter test

--- src/test/java/edu/uco/cs/group3_spring2020/kwic/domain/filter/CircularShiftFilterTest.java
+++ src/test/java/edu/uco/cs/group3_spring2020/kwic/domain/filter/CircularShiftFilterTest.java
@@ -22,7 +22,8 @@ public class CircularShiftFilterTest {
     final Line[] testInput = new Line[] {
         new Line("this is the first test"),
         new Line("here's another test"),
-        new Line("blah blah third test")
+        new Line("blah blah third test"),
+        new Line("abcde 12345 67wx yz89")
     };
     
     final Line[] expectedOutput = new Line[] {
@@ -37,7 +38,11 @@ public class CircularShiftFilterTest {
         new Line("blah blah third test"),
         new Line("blah third test blah"),
         new Line("third test blah blah"),
-        new Line("test blah blah third")
+        new Line("test blah blah third"),
+        new Line("abcde 12345 67wx yz89"),
+        new Line("12345 67wx yz89 abcde"),
+        new Line("67wx yz89 abcde 12345"),
+        new Line("yz89 abcde 12345 67wx")
     };
     
     Filter filter = new CircularShiftFilter();
