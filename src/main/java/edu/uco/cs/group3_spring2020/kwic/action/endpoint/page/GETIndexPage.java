bdc65d53cd169611bef1d540cbde902e4e020bb7
Caleb L. Power <caleb.power@axonibyte.com>
2020-02-22 18:12:33 -0600

    package/file refactor; implemented dummy API for POST /api/content

---
+++ src/main/java/edu/uco/cs/group3_spring2020/kwic/action/endpoint/page/GETIndexPage.java
@@ -0,0 +1,38 @@
+package edu.uco.cs.group3_spring2020.kwic.action.endpoint.page;
+
+import java.util.HashMap;
+
+import edu.uco.cs.group3_spring2020.kwic.action.HTTPMethod;
+import edu.uco.cs.group3_spring2020.kwic.action.endpoint.Endpoint;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+/**
+ * Retrieves the index page.
+ * 
+ * @author Caleb L. Power
+ */
+public class GETIndexPage extends Endpoint {
+
+  /**
+   * Null constructor to initialize the index page and set the appropriate HTTP request type.
+   */
+  public GETIndexPage() {
+    super("/", HTTPMethod.GET); // this page should only be accessible via GET
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public ModelAndView customAction(Request request, Response response) {
+    HashMap<String, Object> model = new HashMap<String, Object>() {
+      private static final long serialVersionUID = -896727886628790065L; {
+        put("title", "Index Page");
+        put("content", "Hello, world!");
+    }};
+
+    return new ModelAndView(model, "index.ftl"); // use the index template to generate the output
+  }
+
+}
