ec6e03ea6c953341f45ac5ac80414dab3f2916a8
comphonia <online.comphonia@gmail.com>
2020-04-18 23:26:55 -0500

    view updated

--- frontend/src/main/resources/responder/static/index.js
+++ frontend/src/main/resources/responder/static/index.js
@@ -1,84 +1,116 @@
 $(document).ready(function () {
-    start();
+  start();
 
-    const baseUrl = "http://localhost:4567/api/content";
+  const baseUrl = "http://localhost:4567/api/content";
 
-    function start() {
-        $("#code").focus();
-        addListeners();
-    }
+  function start() {
+    $("#code").focus();
+    addListeners();
+  }
 
-    function addListeners() {
-        $("#code").on("input", function () {
-            if (getContent().length === 0) {
-                $('.line').html('&nbsp;')
-            }
-        });
-        $("#submit").on('click', function () {
-            const content = getContent();
-            parseToKWIC(content);
-        });
-        $("#clear").on('click', function () {
-          $("#code").html(`<div class="line">&nbsp;</div>`)
-        });
-    }
+  function addListeners() {
+    $("#code").on("input", function () {
+      if (getContent().length === 0) {
+        $(".line").html("&nbsp;");
+      }
+    });
+    $("#submit").on("click", function () {
+      const content = getContent();
+      parseToKWIC(content);
+    });
+    $("#saveBtn").on("click", function () {
+        const input = $("#db-content").val().trim()
+        if(input.length > 0)
+        saveInputToDb(input)
+        else
+        showAlert("warning", "A valid text input is required");  
+    });
+  }
 
-    function getContent() {
-        // get all lines and push them to content
-        const content = [];
-        $(".line").each(function () {
-            const text = $(this).text().trim()
-            if(text) content.push(text)
-        });
+  async function saveInputToDb(input) {
+      const entries = []
 
-        return content;
-    }
-
-    function setPreviewContent(text) {
-        $("#preview").html(text);
-    }
-
-    async function parseToKWIC(content) {
-        if(content.length <= 0){
-            showAlert('warning','A valid text input is required')
-            return;
+      try {
+        const lines = input.split("\n")
+      
+        for(line of lines){
+            const data = line.match(/([\w*|\d*|\s{1}]+)(http|https)(:\/\/[\w|\.|\d]+)/)
+            entries.push({url:data[2].toString().trim()+data[3].toString().trim(), description:data[1].toString().trim()})
         }
+      console.log(JSON.stringify({entries}))
+      } catch (error) {
+        return showAlert("warning", "Invalid input format");
+      }
 
-        $('#loader').toggleClass('hidden')
+    try {
+        $("#loader").toggleClass("hidden");
+      const response = await fetch(baseUrl, {
+        method: "post",
+        body: JSON.stringify({entries}),
+      });
+      const json = await response.json();
+      console.log(json)
+    } catch (e) {
+      showAlert("danger", e);
+    } finally {
+      $("#loader").toggleClass("hidden");
+    }
+  }
 
-        try {
-            const response = await fetch(baseUrl, {
-                method: "post",
-                body: JSON.stringify({lines: content})
-            })
-            const json = await response.json()
-            let lines = ""
+  function getContent() {
+    // get all lines and push them to content
+    const content = [];
+    $(".line").each(function () {
+      const text = $(this).text().trim();
+      if (text) content.push(text);
+    });
 
-            for (const line of json.lines) {
-                lines += `<div>${tagStripper(line)}</div>`
-            }
-            setPreviewContent(lines);
+    return content;
+  }
 
-        } catch (e) {
-            showAlert('danger',e)
-        } finally {
-            $('#loader').toggleClass('hidden')
-        }
+  function setPreviewContent(text) {
+    $("#preview").html(text);
+  }
+
+  async function parseToKWIC(content) {
+    if (content.length <= 0) {
+      showAlert("warning", "A valid text input is required");
+      return;
     }
 
-    function showAlert(type, msg){
-        $('#alert').html(`
+    try {
+        $("#loader").toggleClass("hidden");
+      const response = await fetch(baseUrl, {
+        method: "post",
+        body: JSON.stringify({ lines: content }),
+      });
+      const json = await response.json();
+      let lines = "";
+
+      for (const line of json.lines) {
+        lines += `<div>${tagStripper(line)}</div>`;
+      }
+      setPreviewContent(lines);
+    } catch (e) {
+      showAlert("danger", e);
+    } finally {
+      $("#loader").toggleClass("hidden");
+    }
+  }
+
+  function showAlert(type, msg) {
+    $("#alert").html(`
             <div class="alert alert-${type} alert-dismissible fade show" role="alert">
                 <strong>Error!</strong> ${msg}
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            `)
-    }
+            `);
+  }
 
-    // utilities
-    const tagStripper = html => {
-        return html.replace(/<\/?[^>]+(>|$)/g, '');
-    };
+  // utilities
+  const tagStripper = (html) => {
+    return html.replace(/<\/?[^>]+(>|$)/g, "");
+  };
 });
